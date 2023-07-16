3204252eb5a240dd4c9f2b3fd24bcac54b780a27
Tawfiq Hasan <tawfiqhasan58@gmail.com>
2023-07-17 00:35:14 +0600

    feat: add side and navbar

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-export default function HomePage() {
+import Sidebar from '../components/Sidebar';
+import Navbar from '../components/Navbar';
+
+function Dashboard() {
+
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
   return (
-    <div>HomePage</div>
-  )
+    <div className="flex h-screen overflow-hidden">
+
+      <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+
+      <div className="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
+
+        <Navbar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+
+        <main>
+          <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
+
+
+            <div className="grid grid-cols-12 gap-6">
+                Your cards are here!
+
+            </div>
+
+          </div>
+        </main>
+      </div>
+    </div>
+  );
 }
+
+export default Dashboard;
