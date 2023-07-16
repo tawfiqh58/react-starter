9d95971f9541f8b3d653be953da5a9d57d7a490c
Tawfiq Hasan <tawfiqhasan58@gmail.com>
2023-07-16 23:54:13 +0600

    feat: route change to scroll the page to top

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 
 function App() {
+  const location = useLocation();
+
+  useEffect(() => {
+    document.querySelector('html').style.scrollBehavior = 'auto'
+    window.scroll({ top: 0 })
+    document.querySelector('html').style.scrollBehavior = ''
+  }, [location.pathname]); // triggered on route change
+
   return (
     <BrowserRouter>
       <Routes>
